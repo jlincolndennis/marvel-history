ab3a2a02faa19ad6b96d933ec3e54d4abf6525e6
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2016-02-28 13:47:06 -0700

    Things are pretty darn mocked up!

--- mockup/app.js
+++ mockup/app.js
@@ -1,28 +1,58 @@
 $(function () {
   console.log("Let's Do This!");
+  
   var monthNames = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 
-    var today = new Date();
-    var day = $("#day");
-    var month = $('#month')
-    var thisDate = (today.getDate())-1;
-    var thisMonth = (today.getMonth());
+  var today = new Date();
+  var day = $("#day");
+  var month = $('#month')
+  var thisDate = (today.getDate())-1;
+  var thisMonth = (today.getMonth());
 
   for (var d = 0; d < 31; d++) {
     day.append($('<option>').attr("value",(d+1)).text(d+1));
-    };
+  };
 
   $(day.children()[thisDate]).attr('selected', true);
 
   for (var m = 0; m < 12; m++) {
     month.append($('<option>').attr('value', (m+1)).text(monthNames[m]));
   };
+
   $(month.children()[thisMonth]).attr('selected', true);
 
-  $('#about').on('click', function () {
-    $('form').fadeToggle();
-    $('.info').fadeToggle();
+  $('#about-button').on('click', function (event) {
+    event.preventDefault();
+    $('.about').toggle();
+    $('.help, .pull-list').hide();
   });
 
+  $('#help-button').on('click', function (event) {
+    event.preventDefault();
+    $('.help').toggle();
+    $('.about, .pull-list').hide();
+  });
+
+  $('#pull-list-button').on('click', function (event) {
+    event.preventDefault();
+    $('.pull-list').toggle();
+    $('.help, .about').hide();
+  });
+
+  $('#fake-search-button').on('click', function (event){
+    event.preventDefault();
+    $('.about, .help, .pull-list').hide();
+    $('.age-results').show();
+  })
+
+  $('.pull-button').on('click', function () {
+    $(this).hide()
+    $(this).next('.unpull-button').css('display', 'inline-block')
+  })
+
+  $('.unpull-button').on('click', function () {
+    $(this).hide()
+    $(this).prev('.pull-button').show()
+  })
 
 })
