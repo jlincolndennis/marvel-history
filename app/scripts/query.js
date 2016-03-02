144552582fbaf6befcd9a4a367aca7e15e346095
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2016-03-02 15:53:54 -0700

    All Calls Back On

--- app/scripts/query.js
+++ app/scripts/query.js
@@ -276,28 +276,28 @@ $(function() {
 
     }
     function modernCall (){
-    // marvelCall(modernPLow, modernPHigh, "modern");
-    // marvelCall(modernOLow, modernOHigh, "modern");
-    // marvelCall(modernNLow, modernNHigh, "modern");
-    // marvelCall(modernMLow, modernMHigh, "modern");
-    // marvelCall(modernLLow, modernLHigh, "modern");
-    // marvelCall(modernKLow, modernKHigh, "modern");
-    // marvelCall(modernJLow, modernJHigh, "modern");
-    // marvelCall(modernILow, modernIHigh, "modern");
-    // marvelCall(modernHLow, modernHHigh, "modern");
-    // marvelCall(modernGLow, modernGHigh, "modern");
-    // marvelCall(modernFLow, modernFHigh, "modern");
-    // marvelCall(modernELow, modernEHigh, "modern");
-    // marvelCall(modernDLow, modernDHigh, "modern");
-    // marvelCall(modernCLow, modernCHigh, "modern");
-    // marvelCall(modernBLow, modernBHigh, "modern");
-    // marvelCall(modernALow, modernAHigh, "modern");
+    marvelCall(modernPLow, modernPHigh, "modern");
+    marvelCall(modernOLow, modernOHigh, "modern");
+    marvelCall(modernNLow, modernNHigh, "modern");
+    marvelCall(modernMLow, modernMHigh, "modern");
+    marvelCall(modernLLow, modernLHigh, "modern");
+    marvelCall(modernKLow, modernKHigh, "modern");
+    marvelCall(modernJLow, modernJHigh, "modern");
+    marvelCall(modernILow, modernIHigh, "modern");
+    marvelCall(modernHLow, modernHHigh, "modern");
+    marvelCall(modernGLow, modernGHigh, "modern");
+    marvelCall(modernFLow, modernFHigh, "modern");
+    marvelCall(modernELow, modernEHigh, "modern");
+    marvelCall(modernDLow, modernDHigh, "modern");
+    marvelCall(modernCLow, modernCHigh, "modern");
+    marvelCall(modernBLow, modernBHigh, "modern");
+    marvelCall(modernALow, modernAHigh, "modern");
   }
     modernCall();
 
     marvelCall(bronzeLow, bronzeHigh, "bronze");
-    // marvelCall(silverLow, silverHigh, "silver");
-    // marvelCall(goldenLow, goldenHigh, "golden");
+    marvelCall(silverLow, silverHigh, "silver");
+    marvelCall(goldenLow, goldenHigh, "golden");
 
     // // Sort Modern Age Results by Year
     // console.log($('.modern .issue'));
